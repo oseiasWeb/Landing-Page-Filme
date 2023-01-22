ff3b1b9898c7e78548ef8d990fd69c3772b80de3
Oseias Nogueira <oseias.n.m@gmail.com>
2023-01-21 23:26:41 -0300

    Criação da constante que fecha o trailer

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,8 @@ const modal = document.querySelector(".modal");
 botaoTrailer.addEventListener("click", () => {  
     modal.classList.add("aberto");
 });
+
+const botaoFecharModal = document.querySelector(".fechar-modal");
+botaoFecharModal.addEventListener("click", () => {
+    modal.classList.remove("aberto");
+})
