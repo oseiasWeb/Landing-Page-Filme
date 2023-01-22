d7a9ade5da9b562115a42dd98703daa0029fbfb7
Oseias Nogueira <oseias.n.m@gmail.com>
2023-01-21 23:48:41 -0300

    Criação do atributo que restaura o link do trailer

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,15 @@
 const botaoTrailer = document.querySelector(".botao-trailer"); 
 const modal = document.querySelector(".modal");
+const video = document.getElementById("video");
+const linkVideo = video.src;
 
 botaoTrailer.addEventListener("click", () => {  
     modal.classList.add("aberto");
+    video.setAttribute("src",linkVideo);
 });
 
 const botaoFecharModal = document.querySelector(".fechar-modal");
 botaoFecharModal.addEventListener("click", () => {
     modal.classList.remove("aberto");
+    video.setAttribute("src","");
 })
